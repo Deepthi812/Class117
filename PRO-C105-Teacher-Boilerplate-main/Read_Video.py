64720ef3a0ea136f79d8d2f637a5570078b9754d
Deepthi812 <98953840+Deepthi812@users.noreply.github.com>
2023-11-03 17:54:19 +0530

    Add files via upload

---
+++ PRO-C105-Teacher-Boilerplate-main/Read_Video.py
@@ -0,0 +1,27 @@
+import cv2
+vid=cv2.VideoCapture("AnthonyShkraba.mp4")
+
+
+if(vid.isOpened()==False):
+    print("Unable to read the feed")
+
+
+height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+print(height)
+width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+print(width)
+fps =  int(vid.get(cv2.CAP_PROP_FPS))
+print(fps)
+
+out = cv2.VideoWriter('Boxing.mp4',cv2.VideoWriter_fourcc(*'DIVX'), 50, (width,height))
+
+while(True):
+    ret, frame = vid.read()
+
+    cv2.imshow("Web cam", frame)
+    out.write(frame)
+    if cv2.waitKey(0) == 32:
+        break
+vid.release()
+out.release()
+cv2.destroyAllWindows()
